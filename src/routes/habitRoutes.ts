79861cc83a6d5614c153f9cec6e73a0ae20646aa
DhananjayaYapa <it21185052@my.sliit.lk>
2026-01-18 17:14:54 +0530

    setup the middlewares

--- src/routes/habitRoutes.ts
+++ src/routes/habitRoutes.ts
@@ -1,22 +1,35 @@
 import { Router } from 'express'
+import { validateBody, validateParams } from '../middleware/validation.ts'
+import z from 'zod'
 
 const router = Router()
+const createHabitSchema = z.object({
+  name: z.string(),
+})
+const completedParamsSchema = z.object({
+  id: z.string().max(3),
+})
 
 router.get('/', (req, res) => {
   res.json({ message: 'Get all posts' })
 })
 
-router.get('/:id', (req, res) => {
+router.get('/:id', validateParams(completedParamsSchema), (req, res) => {
   res.json({ message: 'Get one habit' })
 })
 
-router.post('/', (req, res) => {
+router.post('/', validateBody(createHabitSchema), (req, res) => {
   res.status(201).json({ message: 'Habit created' })
 })
 
-router.post('/:id/complete', (req, res) => {
-  res.status(201).json({ message: 'Habit completed' })
-})
+router.post(
+  '/:id/complete',
+  validateParams(completedParamsSchema),
+  validateBody(createHabitSchema),
+  (req, res) => {
+    res.status(201).json({ message: 'Habit completed' })
+  },
+)
 
 router.delete('/:id', (req, res) => {
   res.json({ message: 'Habit is deleted' })
--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,21 @@ import express from 'express'
 import authRoutes from './routes/authRoutes.ts'
 import habitRoutes from './routes/habitRoutes.ts'
 import userRoutes from './routes/userRoutes.ts'
+import cors from 'cors'
+import helmet from 'helmet'
+import morgan from 'morgan'
+import { isTest } from '../env.ts'
 
 const app = express()
+app.use(helmet())
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(
+  morgan('dev', {
+    skip: () => isTest(),
+  }),
+)
 
 app.post('/cake', (req, res) => {
   res.send('Get one user').status(201)
